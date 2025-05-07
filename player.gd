b085b33b9db2d1af27678dda6b816e22d06496ef
Faseeh <faseeh1080@gmail.com>
2025-05-07 12:02:48 +0530

    Export settings

--- player.gd
+++ player.gd
@@ -38,7 +38,7 @@ func _process(delta):
 		$AnimatedSprite2D.flip_v = velocity.y > 0
 
 
-func _on_body_entered(body: Node2D) -> void:
+func _on_body_entered(_body: Node2D) -> void:
 	hide()
 	hit.emit()
 	# Must be deferred as we can't change physics properties on a physics callback.
