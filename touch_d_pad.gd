0d96e8ccf6b902a841a0b65e056a0e01452568ed
Faseeh <faseeh1080@gmail.com>
2025-05-07 16:23:01 +0530

    Create touch_d_pad

---
+++ touch_d_pad.gd
@@ -0,0 +1,18 @@
+extends CanvasLayer
+
+var direction = Vector2.ZERO
+
+func _process(delta: float) -> void:
+	var new_direction = Vector2.ZERO
+	
+	if $up.is_pressed():
+		new_direction.y += 1
+	if $down.is_pressed():
+		new_direction.y -= 1
+	if $right.is_pressed():
+		new_direction.x += 1
+	if $left.is_pressed():
+		new_direction.x -= 1
+	
+	new_direction = new_direction.normalized()
+	direction = new_direction
