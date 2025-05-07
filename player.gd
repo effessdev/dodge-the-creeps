60ca61a3034ce84a9dd67f4f3c87cb413031294c
Faseeh <faseeh1080@gmail.com>
2025-05-07 15:14:48 +0530

    Add comment

--- player.gd
+++ player.gd
@@ -39,6 +39,7 @@ func _process(delta):
 
 
 func _on_body_entered(_body: Node2D) -> void:
+	# Note: _ before the body parameter indicates it's unused (convension).
 	hide()
 	hit.emit()
 	# Must be deferred as we can't change physics properties on a physics callback.
